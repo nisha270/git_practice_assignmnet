8f94549a80ded760d8dcd381202cd49997c6901b
Golugauraw1 <kavermaji89@gmail.com>
2022-09-14 18:27:19 +0530

    upadated

--- code.js
+++ code.js
@@ -1,4 +1,8 @@
-let b=10;
-let a=4;
-let sum=a+b;
-console.log(sum);
+let arr=[1,2,3,4,5,6];
+let sum=0;
+let count=0;
+for(let i=0;i<=arr.length-1;i++){
+    sum=sum+arr[i];
+    count++
+}
+console.log(Math.ceil(sum/count))
